aefabf37c7c7f9ce5270182e142851f943aabd7e
Luca Matei <dev@lucamatei.eu>
2024-05-08 23:16:22 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="shopify",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Luca Matei",
+    author_email="dev@lucamatei.eu",
+    description="A simple Shopify API client",
+    url="https://github.com/lucamatei/shopify",
+)
